fca8299d17dc7cb6ab7c66df38bd3cdf0fd96c78
Omid Rajabi <omid.345@gmail.com>
2017-07-10 12:46:08 -0400

    completed Lesson03, total 7 exercises

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -118,15 +118,20 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     }
 
     // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
-
-    // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
-
-    // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
-    // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
-
-    // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
-    // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
-    // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
-
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+        // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
+        TextView listItemNumberView;
+        // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
+        // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+        // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
+        void bind(int listIndex) {
+            // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
+            // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
     }
 }
--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,8 +83,27 @@ public class MainActivity extends AppCompatActivity {
     // TODO (8) Use getMenuInflater().inflate to inflate the menu
     // TODO (9) Return true to display this menu
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
     // TODO (10) Override onOptionsItemSelected
     // TODO (11) Within this method, get the ID from the MenuItem
     // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
     // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_refresh) {
+            mAdapter = new GreenAdapter(100);
+            mNumbersList.setAdapter(mAdapter);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
